a8c3a51fff3667e2b13895d897b7b810645b9971
hugh.nelson <hugh.nelson@student.education.wa.edu.au>
2021-09-16 09:58:27 +0800

    Deleted rectangle testing, started making game: - background - apples - start of class for player

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 # Basic Pygame Structure
 
 import pygame                               # Imports pygame and other libraries
-
+import random
 # Define Classes (sprites) here
+class FallingObject(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.timecreated = pygame.time.get_ticks() # Time object is created
+        self.image = pygame.Surface([30,30]) # creates surface
+        self.image.set_colorkey(black) # makes in transparent
 
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0,670)
+        self.rect.y = 0
+
+    def setImage(self, graphicSelected):
+        allFallingObjectsImage = pygame.image.load(graphicSelected)
+        self.image.blit(allFallingObjectsImage,(0,0))
+
+    def moveFallingObjects(self, distance):
+        if self.rect.y <= 670:
+            self.rect.y = self.rect.y  + distance
+            
+    def deleteFallingObjects(self):
+        if self.rect.y > 470:
+            self.kill()
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
 pygame.init()                               # Pygame is initialised (starts running)
 
 screen = pygame.display.set_mode([700,500]) # Set the width and height of the screen [width,height]
-pygame.display.set_caption("Hugh-Nelson.game")       # Name your window
+pygame.display.set_caption("dodge")       # Name your window
+background = pygame.image.load("OrchardBackground.jpg").convert()
 done = False                                # Loop until the user clicks the close button.
 clock = pygame.time.Clock()                 # Used to manage how fast the screen updates
 black    = (0, 0, 0)                 # Define some colors using rgb values.  These can be
 white    = ( 255, 255, 255)                 # used throughout the game instead of using rgb values.
 speed = 10
-rect_x, rect_y = 40,40
 # Define additional Functions and Procedures here
+allFallingObjects = pygame.sprite.Group()
 
+nextApple = pygame.time.get_ticks() + 2500
 # -------- Main Program Loop -----------
 while not done:
 
@@ -24,12 +51,20 @@ while not done:
             done = True                     # Flag that we are done so we exit this loop
 
     # Update sprites here
-    rect_x += 1
-    rect_y -= 2
-    screen.fill(black)
+    if pygame.time.get_ticks() > nextApple:
+        nextObject = FallingObject()
 
-    pygame.draw.rect(screen, white, pygame.Rect(rect_x, rect_y, 100, 160))
+        nextObject.setImage("Apple.png")
+        allFallingObjects.add(nextObject)
+        nextApple = pygame.time.get_ticks() + 1500
 
+
+    for eachObject in (allFallingObjects.sprites()):
+        eachObject.moveFallingObjects(5)
+
+        eachObject.deleteFallingObjects()
+    screen.blit(background, [0,0])
+    allFallingObjects.draw(screen)
     pygame.display.flip()                   # Go ahead and update the screen with what we've drawn.
     clock.tick(20)                          # Limit to 20 frames per second
 
