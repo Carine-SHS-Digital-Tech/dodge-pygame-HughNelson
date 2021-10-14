4d21609aa360f453918644bb321d6a2d425c95b4
hugh.nelson <hugh.nelson@student.education.wa.edu.au>
2021-10-14 09:23:44 +0800

    made majority of Pong game

--- Kidscancode tutorial.py
+++ Kidscancode tutorial.py
@@ -1,24 +1,38 @@
 import pygame
 import random
 
-class Player(pygame.sprite.Sprite):
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 50))
         self.image.fill((50,255,10))
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(1,width-55),random.randint(1,height-55))
+        self.rect.center = (width/2,height/2)
         self.x_speed = 1
     def update(self):
 
 
-        if self.rect.x <= 0:
+        if self.rect.colliderect(Player()):
             self.x_speed *= -1
         if self.rect.x >= width-50:
             print("passed")
             self.x_speed *= -1
 
         self.rect.x += self.x_speed
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 100))
+        self.image.fill((50,255,10))
+        self.rect = self.image.get_rect()
+        self.rect.center = (30,height/2)
+        self.x_speed = 1
+
+    def move(self, direction):
+        if direction == "down":
+            self.rect.y += 1
+
+
 
 pygame.init()
 pygame.mixer.init()
@@ -31,19 +45,28 @@ pygame.display.set_caption("My game")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+ball = Ball()
 player = Player()
-
-all_sprites.add(player)
+all_sprites.add(ball)
 
 
 running = True
 
 while running:
+
+
     clock.tick(200)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player.move("down")
+            if event.key == pygame.K_UP:
+                player.move("up")
+
     # Update
+    all_sprites.add(ball)
     all_sprites.add(player)
 
     all_sprites.update()
